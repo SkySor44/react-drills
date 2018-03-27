e6a9da3fe0970a85624e506b0fc04ef9d332be03
Skyler Sorensen <skylersorensen@Skylers-MacBook-Air.local>
2018-03-27 15:42:56 -0600

    finished

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios'
 class App extends Component {
+  constructor(){
+    super()
+    this.state ={
+      baseUrl: 'https://swapi.co/api/people/4/',
+      darth: '' 
+    }
+  }
+  
+  componentDidMount(){
+    axios.get(this.state.baseUrl).then(res => {
+      this.setState({
+        darth: res.data
+      })
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Name: {this.state.darth.name}</h1>
+        <h2>Skin Color: {this.state.darth.skin_color}</h2>
+        <h2>Eye Color: {this.state.darth.eye_color}</h2>
       </div>
     );
   }
